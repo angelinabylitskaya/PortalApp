b7cb9320913bb038b96eefd657c4731e963703e1
Angelina Bylitskaya <angelina.bylitskaya@hiqo-solutions.com>
2024-08-29 16:44:32 +0300

    Create src folder.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
-const colors = require("./constants/_colors.js");
+const colors = require("./src/constants/_colors.js");
 
 /** @type {import("tailwindcss").Config} */
 module.exports = {
-  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/app/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {
       fontFamily: {
